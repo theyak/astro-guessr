2f9bc525d48ca17aa1319602e7dd35e4a5e616aa
larbear <evans@artofproblemsolving.com>
2023-09-30 19:45:25 -0700

    Fix a couple bugs. Allow localstorage user_token

--- src/pages/get-locations.js
+++ src/pages/get-locations.js
@@ -6,7 +6,15 @@
 
 import {connect} from "@planetscale/database";
 
-export async function GET({params, request}) {
+export async function GET({request}) {
+	const url = new URL(request.url);
+	const queryParams = new URLSearchParams(url.search);
+	const token = queryParams.get("token");
+
+	if (!token) {
+		return new Response(JSON.stringify({success: false, message: "Invalid token"}), { status: 400 });
+	}
+
 	const env = import.meta.env;
 	const config = {
 		host: env.DATABASE_HOST,
@@ -16,7 +24,7 @@ export async function GET({params, request}) {
 
 	const conn = await connect(config);
 
-	const results = await conn.execute('SELECT * FROM locations WHERE user_token = ?', ["GRc202d76ce7dd7724a8182b3ab7ab5b"]);
+	const results = await conn.execute('SELECT * FROM locations WHERE user_token = ?', [token]);
 
 	return new Response(JSON.stringify(results.rows));
 }
